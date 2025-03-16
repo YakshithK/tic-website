02131edf090bffbd6c0cf9075ca4cb37100632b4
Yakshith Kommineni <Yakshith.kommineni@gmail.com>
2025-03-15 20:53:25 -0400

    fixed meta desc

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'TIC',
-  description: 'Created with v0',
+  description: 'Toronto Innovation Challenge',
   generator: 'v0.dev',
 }
 
